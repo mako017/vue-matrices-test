0e686b5f60e17ef133a2d7f6c9e08a59906f0e34
mako017 <54231461+mako017@users.noreply.github.com>
2020-06-17 16:57:11 +0200

    fix vue config

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,3 @@
 module.exports = {
-    publicPath: process.env.NODE_ENV === 'production'
-      ? '/vue-matrices/'
-      : '/'
-  }
+  publicPath: process.env.NODE_ENV === "production" ? "/vue-matrices-test/" : "/",
+};
