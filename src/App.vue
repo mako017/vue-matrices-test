c3f22d2211468e83884dc91ed199c35391d80755
mako017 <54231461+mako017@users.noreply.github.com>
2020-07-06 14:25:10 +0200

    downloading of test settings

--- src/App.vue
+++ src/App.vue
@@ -1,34 +1,41 @@
 <template>
-    <div id="app">
-        <router-view />
-    </div>
+  <div id="app">
+    <router-view />
+  </div>
 </template>
 
 <script>
-require('@/assets/css/normal.css');
+import COMM from "@/assets/js/communications.js";
+require("@/assets/css/normal.css");
+
+export default {
+  beforeMount() {
+    COMM.sendData(1, "readTestSet");
+  },
+};
 </script>
 
 <style scoped>
 #app {
-    font-family: Avenir, Helvetica, Arial, sans-serif;
-    -webkit-font-smoothing: antialiased;
-    -moz-osx-font-smoothing: grayscale;
-    text-align: center;
-    color: #2c3e50;
-    height: 100vh;
-    padding: 0 20%;
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  height: 100vh;
+  padding: 0 20%;
 }
 
 #nav {
-    padding: 30px;
+  padding: 30px;
 }
 
 #nav a {
-    font-weight: bold;
-    color: #2c3e50;
+  font-weight: bold;
+  color: #2c3e50;
 }
 
 #nav a.router-link-exact-active {
-    color: #42b983;
+  color: #42b983;
 }
 </style>
---
+++ src/assets/js/communications.js
@@ -0,0 +1,23 @@
+import axios from "axios";
+
+class COMM {
+  static PHPDIR = "https://lets-test.it/matrices/assets/php/";
+  static async sendData(data, code) {
+    let sendObj = {
+      data,
+      code,
+    };
+    let response = await axios.post(this.PHPDIR + "readData.php", JSON.stringify(sendObj));
+    console.log(response);
+  }
+  static async requestData(code) {
+    let sendObj = {
+      data: 0,
+      code,
+    };
+    let response = await axios.post(this.PHPDIR + "readData.php", JSON.stringify(sendObj));
+    return response.data;
+  }
+}
+
+export default COMM;
