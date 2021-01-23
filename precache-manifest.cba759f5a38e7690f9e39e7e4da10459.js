968e07ed055187ba4b4725b54e5f64511ccd0f2b
Marco Koch <marco.koch@uni-saarland.de>
2021-01-23 14:21:47 +0100

    deploy

---
+++ precache-manifest.cba759f5a38e7690f9e39e7e4da10459.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fee8ba207c3e77c92d96",
+    "url": "/vue-matrices-test/css/app.0ecdb380.css"
+  },
+  {
+    "revision": "2fcf594daee0e9a7bd39",
+    "url": "/vue-matrices-test/css/chunk-5db295bf.06909a8a.css"
+  },
+  {
+    "revision": "eb818e93e828efc97842",
+    "url": "/vue-matrices-test/css/chunk-62d53cfa.f4bb1f66.css"
+  },
+  {
+    "revision": "dceecf2d01f5999462b2",
+    "url": "/vue-matrices-test/css/chunk-be5fe842.c27f9e40.css"
+  },
+  {
+    "revision": "2b42bd027c7fdbb808b03bf209bfff11",
+    "url": "/vue-matrices-test/index.html"
+  },
+  {
+    "revision": "fee8ba207c3e77c92d96",
+    "url": "/vue-matrices-test/js/app.85dc5336.js"
+  },
+  {
+    "revision": "2fcf594daee0e9a7bd39",
+    "url": "/vue-matrices-test/js/chunk-5db295bf.33e7aa8b.js"
+  },
+  {
+    "revision": "eb818e93e828efc97842",
+    "url": "/vue-matrices-test/js/chunk-62d53cfa.fc7ccadb.js"
+  },
+  {
+    "revision": "dceecf2d01f5999462b2",
+    "url": "/vue-matrices-test/js/chunk-be5fe842.23796867.js"
+  },
+  {
+    "revision": "83d65957f23cb2da2894",
+    "url": "/vue-matrices-test/js/chunk-vendors.7d8fcc24.js"
+  },
+  {
+    "revision": "9589a2e769485a9039be09a91e817ced",
+    "url": "/vue-matrices-test/manifest.json"
+  }
+]);
